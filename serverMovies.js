dbff90f1252fe45b3bf9604e6122ba6bae47be81
Alexis Jamin <alexis.jamin@outlook.com>
2017-06-21 15:18:12 +0200

    selim

--- serverMovies.js
+++ serverMovies.js
@@ -1,16 +1,11 @@
 var express = require('express');
+var request = require('request');
 var app = express();
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
-var movieList = [
-{
-	name : "Maleficient",
-	img : "dummy/thumb-3.jpg",
-	description : "Un film qui pète sa mère"
-}
-
-];
+var movieList = [];
+var poster = "https://image.tmdb.org/t/p/w500";
 
 console.log(movieList);
 
@@ -18,10 +13,12 @@ app.get('/home', function (req, res) {
 
 request("https://api.themoviedb.org/3/movie/popular?api_key=54c7f44c2377f1dcb4e6944f8049a6cc&language=fr&page=1", function(error, response, body) {
   body = JSON.parse(body);
-  console.log(body);
-
-
-    res.render('home', {movieList : movieList});
+  console.log(body.results[0]);
+  for (var i = 0; i < body.results.length; i++) {
+  movieList.push({title : body.results[i].title, poster_path : body.results[i].poster_path, overview : body.results[i].overview});
+  }
+    res.render('home', {movieList : movieList, poster : poster});
+});
 });
 
 app.get('/contact', function (req, res) {
